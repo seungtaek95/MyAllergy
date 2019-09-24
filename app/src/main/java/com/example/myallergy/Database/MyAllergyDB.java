9ddb85e09e4583d6145542ed82710da69dd1bd08
seungtaek95 <seungtaek95@gmail.com>
2019-09-25 02:46:25 +0900

    seungtaek

--- app/src/main/java/com/example/myallergy/Database/MyAllergyDB.java
+++
@@ -1,9 +0,0 @@
-package com.example.myallergy.Database;
-
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.RoomDatabase;
-
-@Database(version = 1, entities = {UserAllergyInfo.class})
-abstract class MyAllergyDB extends RoomDatabase {
-
-}
